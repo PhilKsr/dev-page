8482b247ef9a143601660ec5e602d8d8f5e1bd62
Philipp Kaiser <philipp.kaiser@frachtwerk.de>
2022-05-30 22:56:41 +0200

    feat(frontend): finish projects

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
 
 import portraitPic from '../../assets/images/me.jpg'
 
@@ -10,7 +11,7 @@ export default function AboutView() {
       title:
         'Coming from five years of sales experience I realized the need for efficient processes just as transparent communication in software projects.',
     },
-    { title: 'Besides scalable and clean code I focus on exciting customers and users' },
+    { title: 'Besides scalable and clean code I focus on exciting customers and users.' },
     {
       title:
         "Also passionate about travelling, my recent private project is transforming a transporter into a campervan. With the love for nature and outdoor activities I'm excited to explore the wilds of Germany, Europe and the rest of the World.",
@@ -18,7 +19,7 @@ export default function AboutView() {
     {
       title:
         'On my search for silent places far away from citys or traffic jams I always find a lot of inspiration for life, work and new ',
-      link: { title: 'projects', link: '' },
+      link: { title: 'projects.', link: '/projects' },
     },
   ]
   return (
@@ -32,18 +33,19 @@ export default function AboutView() {
           <div className='flex justify-between'>
             <article className='w-2/4 pr-8'>
               <h2 className='pb-8 text-2xl font-bold'>IN HAMBURG WE SAY MOIN.</h2>
+
               {text.map((p, i) => (
                 <p key={i} className='py-4'>
                   {p.title}
                   {p.link && (
-                    <a href={p.link.link} className='underline hover:text-gray-400'>
+                    <Link href={p.link.link} className='underline hover:text-gray-400'>
                       {p.link.title}
-                      {'.'}
-                    </a>
+                    </Link>
                   )}
                 </p>
               ))}
             </article>
+
             <div className='block'>
               <Image src={portraitPic} alt="Moin! It's me!" width={400} height={500} className='w-2/4 pl-8' />
             </div>
--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -1,44 +1,88 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCode } from '@fortawesome/free-solid-svg-icons'
 import Head from 'next/head'
 import Image from 'next/image'
 
-import budgetingApp from '../../assets/images/Budgeting-App.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCode } from '@fortawesome/free-solid-svg-icons'
+
+import budgetingApp from '../../assets/images/budgetingApp.png'
+import quizApp from '../../assets/images/quizApp.png'
+import roAdventure from '../../assets/images/roAdventure.png'
+import done from '../../assets/images/done.png'
 
 export default function ProjectsView() {
+  const projects = [
+    {
+      title: 'Budgeting App',
+      tags: ['Next.js', 'TypeScript', 'Tailwind'],
+      image: budgetingApp,
+      path: 'https://budgeting-app-sigma.vercel.app/',
+      src: 'https://github.com/Philipp-Kaiser/Budgeting-App',
+    },
+    {
+      title: 'RoAdventure',
+      tags: ['React', 'Node.js', 'MongoDB', 'Leaflet', 'Cypress', 'Jest', 'Storybook'],
+      image: roAdventure,
+      path: 'https://roadventure.herokuapp.com/',
+      src: 'https://github.com/Philipp-Kaiser/capstone-project',
+    },
+    {
+      title: 'done',
+      tags: ['Next.js', 'TypeScript', 'Tailwind'],
+      image: done,
+      path: 'https://done-gamma.vercel.app/',
+      src: 'https://github.com/Philipp-Kaiser/done',
+    },
+    {
+      title: 'Quiz-App',
+      tags: ['React', 'Styled-Components', 'Node.js', 'MongoDB'],
+      image: quizApp,
+      path: 'https://quiz-app-new.herokuapp.com/',
+      src: 'https://github.com/Philipp-Kaiser/quiz-app-react',
+    },
+  ]
+
   return (
     <>
       <Head>
         <title>Philipp Kaiser</title>
         <meta name='viewport' content='initial-scale=1.0, width=device-width' />
       </Head>
+
       <main className='flex flex-col items-center mt-52'>
         <div className='w-8/12'>
-          <h2 className='self-start pb-8 text-2xl font-bold'>CHECK OUT MY PROJECTS.</h2>
-          <section
-            className='flex flex-col <div class="max-w-md py-4 px-8 bg-zinc-100 shadow-lg rounded-lg my-20">
-'>
-            <div className='flex justify-center pb-4'>
-              <a href='' className='block w-2/4 '>
-                <Image src={budgetingApp} alt='Budgeting-App' width={16} height={9} layout='responsive' />
-              </a>
-            </div>
+          <h2 className='self-start pb-8 mb-20 text-2xl font-bold'>CHECK OUT MY PROJECTS.</h2>
 
-            <article>
-              <a href='' className='py-4 font-medium underline hover:text-gray-500'>
-                Budgeting App
+          {projects.map((project, i) => (
+            <section key={i} className='flex mb-20 rounded-lg shadow-sm bg-neutral-50'>
+              <a href={project.path} className='block w-2/4'>
+                <Image
+                  src={project.image}
+                  alt='Budgeting-App'
+                  layout='responsive'
+                  className='w-auto h-auto transition duration-500 transform hover:scale-105'
+                />
               </a>
-              <a href='' className='ml-4'>
-                <FontAwesomeIcon icon={faCode} style={{ fontSize: 16 }} className=' hover:text-gray-500' />
-              </a>
-              <div className='py-4'>
-                <div className='px-2 text-sm text-white bg-gray-500 rounded-3xl max-w-min'>React</div>
-              </div>
-              <p className='text-sm'>
-                Collaboration Project with four developers to test out possibilities of Tailwind and Next.js
-              </p>
-            </article>
-          </section>
+
+              <article className='flex flex-col items-center justify-center w-2/4 px-4 '>
+                <div>
+                  <a href={project.path} className='text-xl font-medium underline hover:text-gray-500'>
+                    {project.title}
+                  </a>
+                  <a href={project.src} className='px-4'>
+                    <FontAwesomeIcon icon={faCode} style={{ fontSize: 16 }} className=' hover:text-gray-500' />
+                  </a>
+                </div>
+
+                <div className='flex py-8'>
+                  {project.tags.map((tag, i) => (
+                    <div key={i} className='px-2 mr-2 text-white bg-gray-500 rounded-3xl '>
+                      {tag}
+                    </div>
+                  ))}
+                </div>
+              </article>
+            </section>
+          ))}
         </div>
       </main>
     </>
